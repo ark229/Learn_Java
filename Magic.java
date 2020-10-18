beb56410cf6056250107e2dc0a2d7f90427ab77c
Arnetta <40047791+ark229@users.noreply.github.com>
2020-10-18 00:27:50 -0400

    Add files via upload

---
+++ Magic.java
@@ -0,0 +1,19 @@
+public class Magic {
+    public static void main(String[] args) {
+
+        int myNumber = 2876;
+  /*
+  Variable myNumber:
+  myNumber will be referred to as the original number
+  */
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo / myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive / 6;
+
+        System.out.println(stepSix);
+
+    }
+}
